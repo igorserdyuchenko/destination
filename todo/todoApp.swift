5bab7d5ec0113f3321ac1a8b5d7e857eb8a104e4
Igor Serdyuchenko <iserdyuchenko@sonatype.com>
2025-09-03 11:05:39 +0300

    update to swift

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by iserdyuc on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todoTests/todoTests.swift
@@ -0,0 +1,16 @@
+//
+//  todoTests.swift
+//  todoTests
+//
+//  Created by iserdyuc on 03.09.2025.
+//
+
+import Testing
+
+struct todoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
